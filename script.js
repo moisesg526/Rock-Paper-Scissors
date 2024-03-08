ea6b0ce702379a99a2d17243bbacfa100575548f
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-03-07 23:44:29 -0500

    Added playRound function that takes in the player and computers input

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function getComputerChoice() {
-    let game = ["Rock", "Paper", "Scissors"];
-    let pick = game[Math.floor(Math.random()*3)];
-    console.log(pick)
-    return pick;
+  let game = ["Rock", "Paper", "Scissors"];
+  let pick = game[Math.floor(Math.random() * 3)];
+//   console.log(pick);
+  return pick;
 }
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = "Paper";
+  computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playerSelection);
+}
+
+playRound()
