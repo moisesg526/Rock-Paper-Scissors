112729d0585317ee473d2366acd2c6674a702639
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-03-08 21:02:56 -0500

    Added a prompt box so user can make his selection and added if statements for outcomes when player selects paper

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function getComputerChoice() {
   let game = ["Rock", "Paper", "Scissors"];
   let pick = game[Math.floor(Math.random() * 3)];
-//   console.log(pick);
+  //   console.log(pick);
   return pick;
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = "Paper";
+  playerSelection = prompt("Rock, Paper or Scissors?")
+  let hand = playerSelection.toLowerCase();
   computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playerSelection);
+  console.log(`Computer selects ${computerSelection}`);
+  console.log(`Player selects ${hand}`);
+  if (hand === "paper" && computerSelection === "Rock") {
+    console.log("You win! Paper beats rock");
+  } else if (hand === "paper" && computerSelection === "Scissors") {
+    console.log("You lose! Scissors beats Paper");
+  } else if (hand === "paper" && computerSelection === "Paper") {
+    console.log("Paper, Paper. It's a Tie! Try again.");
+  }
 }
 
-playRound()
+playRound();
